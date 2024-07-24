d0ad2d7d74c0f87c4bf29b763850f6474312e36c
Shubham Dalavi <shubhamdalavi20@gmail.com>
2024-07-24 23:27:36 +0530

    added final code to run mfe on host app

--- projects/host-app/src/app/app.routes.ts
+++ projects/host-app/src/app/app.routes.ts
@@ -1,3 +1,16 @@
+import { loadRemoteModule } from '@angular-architects/module-federation';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+const MFE_APP_HIGH_CHART_URL = "http://localhost:4300/remoteEntry.js";
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () => {
+            return loadRemoteModule({
+                remoteEntry: MFE_APP_HIGH_CHART_URL,
+                remoteName: "mfeApp",
+                exposedModule: "./AppComponent"
+            }).then((m) => m.AppComponent).catch(error => console.log('error is ===> ', error));
+        }
+    }
+];
